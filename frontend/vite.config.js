0e3fc96aa05d4bde9cc421af6deeb33c3f1db8cc
Ankit Jha <jhaankitjha03@gmail.com>
2026-04-17 02:06:52 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://powerbill-backend.onrender.com',
+        target: 'https://electricity-billing-system-yar7.onrender.com',
         changeOrigin: true,
       },
     },
